a74705a8f72a84783b48a8939903e794112e1332
Arsène Fougerouse <arsene582@gmail.com>
2024-01-17 10:35:26 +0100

    feat: change netlify dir
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,17 +1,5 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-import express, { Router } from 'express';
-import serverless from 'serverless-http';
-import { translateWord } from '../../src/utils';
+import { handler } from '../../src/index';
 
-const api = express();
-
-const router = Router();
-router.get('/hello', async (req, res) => {
-  const words = await translateWord();
-  res.send(words);
-});
-
-api.use('/api/', router);
-
-export const handler = serverless(api);
+export { handler };
