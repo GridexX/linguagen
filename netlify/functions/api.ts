04431528dfb791eb6ec016e0ba95878a507b06a7
Arsène Fougerouse <arsene582@gmail.com>
2024-01-17 11:12:25 +0100

    test: netlify deployment
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,5 +1,11 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 
-import { handler } from '../../src/index';
+const api = express();
 
-export { handler };
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
